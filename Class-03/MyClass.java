92e766a131ecb3c9a0c9097c25526ff1eba29a87
Charles José Ferrari <svu938@INF.ELTE.HU>
2018-10-11 09:33:51 +0200

    adding Class 3 examples

---
+++ Class-03/MyClass.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class MyClass{
+	
+	public static void main (String args [])throws Exception{
+		
+		SimpleDateFormat myFormat = new SimpleDateFormat("yyyy/MM/dd");
+		//SimpleDateFormat myFormat = new SimpleDateFormat("ddMMyyyy")
+		//SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy")
+		
+		Date d;
+		
+		d = myFormat.parse("1990/12/30");
+		
+		
+		System.out.println(myFormat.format(d));
+		
+		Person p = new Person("Josh", "Smith");
+		
+		
+		
+		System.out.println(p);
+		
+		//String myAttribute = "whatever";
+		//System.out.println("Hello World" + myAttribute);
+	}
+}
