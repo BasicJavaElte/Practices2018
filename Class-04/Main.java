350a864895d5442a62d0dc030eb83de5c922a308
Charles José Ferrari <svu938@INF.ELTE.HU>
2018-10-18 11:08:58 +0200

    fixing try catch

--- Class-04/Main.java
+++ Class-04/Main.java
@@ -3,29 +3,37 @@ import java.text.SimpleDateFormat;
 
 public class Main
 {
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
         try
         {
             // Constructor with Strings
             DateItinerary d1 = new DateItinerary("20171001", "19900201");
-            System.out.println(d1);
+            System.out.println(d1);			
+		}catch(Exception e){
+			System.out.println("First date was not parsed");
+		}
 
-            // Empty Constructor
-            DateItinerary d2 = new DateItinerary();
-            System.out.println(d2);
+		// Empty Constructor
+		DateItinerary d2 = new DateItinerary();
+		System.out.println(d2);
 
-            // Constructor with dates
-            Date current = new Date();
-            SimpleDateFormat my_format = new SimpleDateFormat("yyyyMMdd");
-            Date goal = my_format.parse("20181111");
-            DateItinerary d3 = new DateItinerary(current, goal);
-            System.out.println(d3);
-        }
-        catch (Exception e)
-        {
+		// Constructor with dates
+		Date current = new Date();
+		SimpleDateFormat my_format = new SimpleDateFormat("yyyyMMdd");
+		
+		
+		Date goal = null;
+		try{
+             goal = my_format.parse("20181111");
+		}catch (Exception e){
             System.out.println("Parse error!");
             System.exit(-1);
         }
+		
+		DateItinerary d3 = new DateItinerary(current, goal);
+		System.out.println(d3);
+        
+        
     }
 }
